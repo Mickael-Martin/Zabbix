a09354d84d8b1d0165fdafcd5346ec5f6814f7d1
Mickaël Martin <mickael@librement-votre.fr>
2017-05-18 10:34:46 +0200

    Create diff menu.inc.php

---
+++ Maintenances/Maintenance One Clic/diff menu.inc.php
@@ -0,0 +1,6 @@
+diff include/menu.inc.php include/menu.inc.php.org
+184,187d183
+<                                 'url' => 'maintenance_oneclic.php',
+<                                 'label' => _('Maintenance one clic')
+<                         ),
+< 			array(
