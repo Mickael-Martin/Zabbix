9a8581aa9adacc9df98749debd9bcb3024ccdd42
Mickaël Martin <mickael@librement-votre.fr>
2017-05-18 10:39:48 +0200

    Create diff events.inc.php

---
+++ Maintenances/No sound for maintenances/diff events.inc.php
@@ -0,0 +1,5 @@
+diff ./include/events.inc.php ./include/events.inc.php.org
+391c391
+< 		'selectHosts' => array('hostid', 'name','maintenance_status'),
+---
+> 		'selectHosts' => array('hostid', 'name'),
