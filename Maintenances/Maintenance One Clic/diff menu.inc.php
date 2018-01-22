5490c1bbad7eb681e2d80e4bb4f85c1b22b7205f
Mickaël Martin <mickael@librement-votre.fr>
2018-01-22 09:53:34 +0100

    update for zabbix 3.4.6

--- Maintenances/Maintenance One Clic/diff menu.inc.php
+++ Maintenances/Maintenance One Clic/diff menu.inc.php
@@ -1,6 +1,13 @@
-diff include/menu.inc.php include/menu.inc.php.org
-184,187d183
-<                                 'url' => 'maintenance_oneclic.php',
-<                                 'label' => _('Maintenance one clic')
-<                         ),
-< 			array(
+--- menu.inc.php.org	2018-01-22 09:50:50.412129442 +0100
++++ menu.inc.php	2018-01-22 09:52:01.088624629 +0100
+@@ -222,6 +222,10 @@
+ 					'url' => 'maintenance.php',
+ 					'label' => _('Maintenance')
+ 				],
++				 [
++                                        'url' => 'maintenance_oneclic.php',
++                                        'label' => _('Maintenance one clic')
++                                ],
+ 				[
+ 					'url' => 'actionconf.php',
+ 					'label' => _('Actions')
