cf09a16430cb8b68d10d3e4c164cb4aa7b40614d
Mickaël Martin <mickael@librement-votre.fr>
2018-04-16 14:26:10 +0200

    update to 3.4.8

--- Maintenances/No sound for maintenances/diff jsrpc.php
+++ Maintenances/No sound for maintenances/diff jsrpc.php
@@ -1,3 +1,13 @@
-123,124d122
-< 					
-< 					if ($host['maintenance_status'] == 1) { $sound='no_sound.wav';}
+diff -u /usr/share/zabbix/jsrpc.php.bak /usr/share/zabbix/jsrpc.php
+--- /usr/share/zabbix/jsrpc.php.bak	2018-04-16 14:24:12.365708789 +0200
++++ /usr/share/zabbix/jsrpc.php	2018-04-16 14:25:10.946153900 +0200
+@@ -121,6 +121,9 @@
+ 					}
+ 
+ 					$url_tr_status = 'tr_status.php?hostid='.$host['hostid'];
++						
++					if ($host['maintenance_status'] == 1) { $sound='no_sound.wav';}
++								
+ 					$url_events = (new CUrl('zabbix.php'))
+ 						->setArgument('action', 'problem.view')
+ 						->setArgument('filter_triggerids[]', $event['objectid'])
