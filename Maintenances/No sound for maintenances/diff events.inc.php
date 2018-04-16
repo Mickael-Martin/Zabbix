677bb58021e834df3a9ac58ed450ef5bc0e59754
Mickaël Martin <mickael@librement-votre.fr>
2018-04-16 14:23:51 +0200

    update to 3.4.8

--- Maintenances/No sound for maintenances/diff events.inc.php
+++ Maintenances/No sound for maintenances/diff events.inc.php
@@ -1,5 +1,12 @@
-diff ./include/events.inc.php ./include/events.inc.php.org
-391c391
-< 		'selectHosts' => array('hostid', 'name','maintenance_status'),
----
-> 		'selectHosts' => array('hostid', 'name'),
+diff -u /usr/share/zabbix/include/events.inc.php.bak /usr/share/zabbix/include/events.inc.php
+--- /usr/share/zabbix/include/events.inc.php.bak	2018-04-16 14:22:43.825029683 +0200
++++ /usr/share/zabbix/include/events.inc.php	2018-04-16 14:23:06.877207291 +0200
+@@ -784,7 +784,7 @@
+ 	$triggerOptions = [
+ 		'filter' => [],
+ 		'skipDependent' => 1,
+-		'selectHosts' => ['hostid', 'name'],
++		'selectHosts' => ['hostid', 'name','maintenance_status'],
+ 		'output' => API_OUTPUT_EXTEND,
+ 		'sortfield' => 'lastchange',
+ 		'sortorder' => ZBX_SORT_DOWN,
