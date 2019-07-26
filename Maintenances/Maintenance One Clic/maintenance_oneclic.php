55cee4683f80f69156afbcc2962d5ccb9e5fdd41
Mickaël Martin <mickael@librement-votre.fr>
2019-07-26 09:38:35 +0200

    add day display

--- Maintenances/Maintenance One Clic/maintenance_oneclic.php
+++ Maintenances/Maintenance One Clic/maintenance_oneclic.php
@@ -1,4 +1,3 @@
-<!DOCTYPE HTML>
 <?php
 /*
  ** Zabbix
@@ -41,55 +40,63 @@ ini_set("display_errors", 1);
 
 
 if (!empty($_POST["host"])){
-	$name=$_POST["host"];
-	$period=$_POST["time"];
-	$description=$_POST["description"];
-	$hosts = API::Host()->get(array(
-				'filter' => array('host' => $name,'name' => $name),
-				'output' => array('hostid'),
-				'searchByAny' => 1,
-				'limit' => 1
-				));
-	$now=time();
-	//$tomorrow=time() + 86400;
-	$tomorrow=time() + $period;
-	$hostid=$hosts[0]['hostid'];
-	$user=CWebUser::$data['alias'];
-	$maintenance=array(
-			'name' => "OneClic - $name by $user for ".gmdate("G",$_POST["time"])."h (".date('Y-m-d H:i:s',$now).")",
-			'active_since' => "$now",
-			'active_till' => "$tomorrow",
-			'maintenance_type' => 0,
-			'hostids' => array("$hostid"),
-			'groupids' => array(),
-			'description' => "$description",
-			'timeperiods' => array(array(
-					'timeperiod_type' =>"0",
-					'start_time' =>"$now",
-					'period' =>"$period",
-					'every' => "1",
-					'dayofweek' => "64"
-					))
+        $name=$_POST["host"];
+        $period=$_POST["time"];
+        $description=$_POST["description"];
+        $hosts = API::Host()->get(array(
+                                'filter' => array('host' => $name,'name' => $name),
+                                'output' => array('hostid'),
+                                'searchByAny' => 1,
+                                'limit' => 1
+                                ));
+        $now=time();
+        //$tomorrow=time() + 86400;
+        $tomorrow=time() + $period;
+        $hostid=$hosts[0]['hostid'];
+        $user=CWebUser::$data['alias'];
+        if ($_POST["time"] == 86400 ){
+                $strDuration="1d";
+        }
+        else
+        {
+                $strDuration=gmdate("G:i",$_POST["time"])."h";
+        }
+        $maintenance=array(
+                        'name' => "OneClic - $name by $user for $strDuration (".date('Y-m-d H:i:s',$now).")",
+                        'active_since' => "$now",
+                        'active_till' => "$tomorrow",
+                        'maintenance_type' => 0,
+                        'hostids' => array("$hostid"),
+                        'groupids' => array(),
+                        'description' => "$description",
+                        'timeperiods' => array(array(
+                                        'timeperiod_type' =>"0",
+                                        'start_time' =>"$now",
+                                        'period' =>"$period",
+                                        'every' => "1",
+                                        'dayofweek' => "64"
+                                        ))
 
-			);
-	//echo "<pre>";var_dump($maintenance);echo "</pre>";
-	$result = API::Maintenance()->create($maintenance);
-	if (!empty($result['maintenanceids'][0])){
-		echo "Maintenance succefully added : <a href='http://zabbix/zabbix/maintenance.php?form=update&maintenanceid=".$result['maintenanceids'][0]."'>Maintenance ".$result['maintenanceids'][0]." </a>"
-			;
-	}else
-	{
-		echo "Error when adding maintenance !";
-	}
+                        );
+        //echo "<pre>";var_dump($maintenance);echo "</pre>";
+        $result = API::Maintenance()->create($maintenance);
+        if (!empty($result['maintenanceids'][0])){
+                echo "Maintenance succefully added : <a href='http://zabbix24/zabbix/maintenance.php?form=update&maintenanceid=".$result['maintenanceids'][0]."'>Maintenance ".$result['maintenanceids'][0]." </a>"
+                        ;
+        }else
+        {
+                echo "Error when adding maintenance !";
+        }
 
 
-}	
+}
 
 //form
 ?>
 <div class="header-title table"><div class="cell"><h1>Maintenance one clic</h1></div></div>
 <br/>
 <form method="post">
+<div id="tabs" class="table-forms-container ui-tabs ui-widget ui-widget-content ui-corner-all" style="visibility: visible;">
 <div id="maintenanceTab" aria-labelledby="tab_maintenanceTab" class="ui-tabs-panel ui-widget-content ui-corner-bottom" role="tabpanel" aria-expanded="true" aria-hidden="false">
 <ul class="table-forms" id="maintenanceFormList">
 <li><div class="table-forms-td-left">
@@ -100,53 +107,53 @@ if (!empty($_POST["host"])){
 <datalist id="host">
 <?php
 $hosts = API::Host()->get(array(
-			'output' => array('host','name'),
-			));
+                        'output' => array('host','name'),
+                        ));
 $arr_hosts=array();
 foreach($hosts as $host){
-	array_push($arr_hosts,$host['host']);
-	array_push($arr_hosts,$host['name']);
+        array_push($arr_hosts,$host['host']);
+        array_push($arr_hosts,$host['name']);
 }
 $arr_hosts=array_unique($arr_hosts);
 sort($arr_hosts);
 foreach($arr_hosts as $name){
-	?><option value="<?php echo $name; ?>" /><?php
+        ?><option value="<?php echo $name; ?>" /><?php
 }
 
 ?>
 </datalist>
 <li>
-	<div class="table-forms-td-left">
-		<label for="time">Time </label>
-	</div>
-	<div class="table-forms-td-right">
-		<select id="time" name="time">
-			<option value="900">15min</option>
-			<option value="1800">30min</option>
-			<option value="3600">1h</option>
-			<option value="7200">2h</option>
-			<option value="14400">4h</option>
-			<option value="86400">24h</option>
-		</select>
-	</div>
+        <div class="table-forms-td-left">
+                <label for="time">Time </label>
+        </div>
+        <div class="table-forms-td-right">
+                <select id="time" name="time">
+                        <option value="1800">30min</option>
+                        <option value="3600">1h</option>
+                        <option value="7200">2h</option>
+                        <option value="14400">4h</option>
+                        <option value="86400">24h</option>
+                </select>
+        </div>
 </li>
 <li>
-	<div class="table-forms-td-left">
-		<label for="description">Description </label>
-	</div>
-	<div class="table-forms-td-right">
-		<textarea id="description" name="description" rows="7" style="width: 480px;" value="<?php echo CWebUser::$data['alias']; ?>"></textarea>
-	</div>
+        <div class="table-forms-td-left">
+                <label for="description">Description </label>
+        </div>
+        <div class="table-forms-td-right">
+                <textarea id="description" name="description" rows="7" style="width: 480px;" value="<?php echo CWebUser::$data['alias']; ?>"></textarea>
+        </div>
 </li>
 </ul>
 <ul class="table-forms">
-	<li>
-		<div class="table-forms-td-left"></div>
-		<div class="table-forms-td-right tfoot-buttons">
-			<button type="submit" value="Add"/>Add</button>
-		</div>
-	</li>
-	</ul>
+        <li>
+                <div class="table-forms-td-left"></div>
+                <div class="table-forms-td-right tfoot-buttons">
+                        <button type="submit" value="Add"/>Add</button>
+                </div>
+        </li>
+        </ul>
+</div>
 </div>
 </form>
 <?php
